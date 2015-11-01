41cb8d8d92f3fc8b9209bac7f85c1d8fbd147ae6
abbottmb12 <abbottmb12@gmail.com>
2015-11-01 09:34:02 -0600

    kk

--- conflictandperspective/passion.js
+++ conflictandperspective/passion.js
@@ -13,21 +13,6 @@ function createCookie(name, value, days) {
     console.log(encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/");
 }
 
-function readCookie(name) {
-    var nameEQ = encodeURIComponent(name) + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name, "", -1);
-}
-
 $(document).ready(function() {
     createCookie("passion",1,1);
 });
